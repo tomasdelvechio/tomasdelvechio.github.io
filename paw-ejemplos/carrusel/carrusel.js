83fe93397562f4e5e2f42251f173ac652f509405
Tomas <tomasdelvechio17@gmail.com>
2016-04-11 18:00:11 -0300

    Se termino de construir el carrusel

--- paw-ejemplos/carrusel/carrusel.js
+++ paw-ejemplos/carrusel/carrusel.js
@@ -1,48 +1,51 @@
 
-var Carrousel = function () {
-    this.items = $("#carrusel ul li");
-    this.activeItem = $("#carrusel ul li:first");
-}
+var Carrousel = function () {}
 
 Carrousel.prototype.setFirstItem = function () {
-    this.activeItem = $("#carrusel ul li:first");
+    carrousel.activeItem = $("#carrusel ul li:first");
 }
 
 Carrousel.prototype.setLastItem = function () {
-    this.activeItem = $("#carrusel ul li:last-child");
+    carrousel.activeItem = $("#carrusel ul li:last-child");
 }
 
 Carrousel.prototype.getItems = function () {
-    return this.items;
+    return carrousel.items;
 }
 
-Carrousel.prototype.changeItem = function() {
-    console.log('Nuevo Item');
-    console.log(this.activeItem);
+Carrousel.prototype.changeItem = function () {
+
+    var newPosition = carrousel.activeItem.position().left;
+
+    $("#carrusel ul").animate({
+        left : - newPosition
+    })
 }
 
-Carrousel.prototype.nextItem = function (event) {
-    console.log(event);
+Carrousel.prototype.nextItem = function () {
     carrousel.activeItem = carrousel.activeItem.next();
-    if (carrousel.activeItem == undefined)
+    if (carrousel.activeItem.length == 0)
         carrousel.setFirstItem()
     carrousel.changeItem();
 }
 
 Carrousel.prototype.prevItem = function () {
-    this.activeItem = this.activeItem.prev();
-    if (this.activeItem == undefined)
-        this.setLastItem()
-    this.changeItem();
+    carrousel.activeItem = carrousel.activeItem.prev();
+    if (carrousel.activeItem.length == 0)
+        carrousel.setLastItem()
+    carrousel.changeItem();
 }
 
-var c = new Carrousel();
+Carrousel.prototype.inicializar = function() {
+    carrousel.items = $("#carrusel ul li");
+    carrousel.activeItem = $("#carrusel ul li:first");
+}
+
+var carrousel = new Carrousel();
 
 window.onload = function() {
-    $('#right-arrow').bind('click', {carrousel: c}, function (event) {
-        var data = event.data;
-        alert(c);
-    })
-    $('#right-arrow').click(c.nextItem);
-    $('#left-arrow').click(c.prevItem);
+    carrousel.inicializar();
+
+    $('#right-arrow').click(carrousel.nextItem);
+    $('#left-arrow').click(carrousel.prevItem);
 }
