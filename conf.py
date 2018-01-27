79925bd694e3bfe89ffadafccd729d0714ecc754
Tomas <tomasdelvechio17@gmail.com>
2018-01-27 15:21:12 -0300

    Nikola auto commit.
    Nikola version: 7.8.11

--- conf.py
+++ conf.py
@@ -1233,7 +1233,17 @@ SHOW_SOURCELINK = False
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-16017127-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-16017127-6');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
